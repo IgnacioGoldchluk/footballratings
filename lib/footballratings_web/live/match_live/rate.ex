38225366cf2b252f058dd3c434615e640df3a4aa
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2023-06-20 19:16:43 -0300

    frontend rate

--- lib/footballratings_web/live/match_live/rate.ex
+++ lib/footballratings_web/live/match_live/rate.ex
@@ -6,44 +6,6 @@ defmodule FootballratingsWeb.MatchLive.Rate do
   alias Footballratings.Ratings
 
   @impl true
-  def render(assigns) do
-    ~H"""
-    <.link navigate={~p"/matches"}>
-      <.button class="btn btn-primary">Back to matches</.button>
-    </.link>
-    <.form for={@scores} phx-submit="submit">
-      <div class="flex flex-col space-y-8">
-        <%= for player <- @players do %>
-          <div class="flex justify-between w-80">
-            <div class="grid grid-rows-2 justify-items-start">
-              <img class="mask mask-circle" src="/images/player.jpg" width="75" />
-              <div class="text-2xl"><%= player.name %></div>
-            </div>
-            <div class="flex items-center justify-center rounded-full border-2 bg-primary w-32 object-over h-24 w-16">
-              <div class="text-2xl text-black justify-center">
-                <%= @scores[player.id] %>
-              </div>
-            </div>
-          </div>
-          <.input
-            type="range"
-            min="1"
-            max="10"
-            value={"#{@scores[player.id]}"}
-            step="1"
-            class="range range-primary w-80 duration-300"
-            name={player.id}
-            field={player.id}
-            phx-change="player_changed"
-          />
-        <% end %>
-      </div>
-      <.button class="btn btn-primary" phx-disable-with="Saving...">Rate players</.button>
-    </.form>
-    """
-  end
-
-  @impl true
   def mount(%{"match_id" => match_id, "team_id" => team_id}, _session, socket) do
     {match_id, team_id} = {String.to_integer(match_id), String.to_integer(team_id)}
     players = Footballratings.FootballInfo.players_for_match(match_id, team_id)
