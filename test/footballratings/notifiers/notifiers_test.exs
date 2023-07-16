a1c0aeeac43c4a0207169f3a2a924b807be02bfd
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2023-07-16 15:27:05 -0300

    add rating notifier test

--- test/footballratings/notifiers/notifiers_test.exs
+++ test/footballratings/notifiers/notifiers_test.exs
@@ -1,5 +1,6 @@
 defmodule Footballratings.Notifiers.NotifiersTest do
   use FootballratingsWeb.ConnCase
+  alias Footballratings.RatingsFixtures
   alias Footballratings.InternalDataFixtures
 
   alias Footballratings.Notifiers
@@ -36,6 +37,28 @@ defmodule Footballratings.Notifiers.NotifiersTest do
       @endpoint.subscribe("team:#{atid}")
 
       Notifiers.Match.broadcast_new_match(match)
+      assert_received(^expected_payload)
+    end
+  end
+
+  describe "rating notifier" do
+    test "broadcast_new_rating/1 broadcasts match topic" do
+      %{team_id: tid, match_id: mid} = match_rating = RatingsFixtures.create_match_ratings()
+
+      expected_payload = %{"type" => "new_rating", "match_id" => mid, "team_id" => tid}
+
+      @endpoint.subscribe("match:#{mid}")
+      Notifiers.Rating.broadcast_new_rating(match_rating)
+      assert_received(^expected_payload)
+    end
+
+    test "broadcast_new_rating/1 broadcasts team topic" do
+      %{team_id: tid, match_id: mid} = match_rating = RatingsFixtures.create_match_ratings()
+
+      expected_payload = %{"type" => "new_rating", "match_id" => mid, "team_id" => tid}
+
+      @endpoint.subscribe("team:#{tid}")
+      Notifiers.Rating.broadcast_new_rating(match_rating)
       assert_received(^expected_payload)
     end
   end
