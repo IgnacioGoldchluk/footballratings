955b837072c22685b4a8176a3b11a00971a795c5
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2023-10-24 14:28:46 -0300

    fix: error messages in OAuth controller

--- lib/footballratings_web/controllers/user_oauth_controller.ex
+++ lib/footballratings_web/controllers/user_oauth_controller.ex
@@ -12,18 +12,14 @@ defmodule FootballratingsWeb.UserOauthController do
     user_params = %{email: user_info.email, password: random_password()}
 
     case Accounts.fetch_or_create_user(user_params) do
-      {:ok, user} ->
-        UsersAuth.log_in_users(conn, user)
-
-      x ->
-        IO.inspect(x)
-        conn |> put_flash(:error, "Authentication fafailed") |> redirect(to: ~p"/")
+      {:ok, user} -> UsersAuth.log_in_users(conn, user)
+      _ -> conn |> put_flash(:error, "Failed fetching/creating user") |> redirect(to: ~p"/")
     end
   end
 
   def callback(conn, _params) do
     conn
-    |> put_flash(:error, "Authentication failed")
+    |> put_flash(:error, "Auth provider did not return valid information")
     |> redirect(to: ~p"/")
   end
 
